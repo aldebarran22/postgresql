91b8ca1f0bd9af1676997aef2a402b9fff1c53b9
aldebarran22 <antonioespinherranz@hotmail.com>
2024-10-22 14:05:13 +0200

    14.05

--- codigo/empresa3_joins.sql
+++ codigo/empresa3_joins.sql
@@ -33,5 +33,20 @@ on c.id = p.idcategoria
 group by 1
 order by 1,2;
 
+-- Número de productos por categoría, NO mostrando las que tienen cero
+select c.nombre as categoria, count(p.id) as cuenta 
+from tbcategorias c right join tbproductos p
+on c.id = p.idcategoria
+group by 1
+order by 1,2;
+
+-- Listado de Categoria, Proveedor y producto:
+select c.nombre as categoria, pr.nombre as proveedor, p.nombre as producto
+from tbcategorias c 
+inner join tbproductos p on c.id = p.idcategoria
+inner join tbproveedores pr on pr.id = p.idproveedor
+order by 1,2,3;
+
+
 
 
