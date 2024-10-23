ebed488fb73f1348eccfa090dd28eeee5c9c96e1
aldebarran22 <antonioespinherranz@hotmail.com>
2024-10-23 14:00:30 +0200

    14.00

--- codigo/consultas_accion_empresa3.sql
+++ codigo/consultas_accion_empresa3.sql
@@ -8,3 +8,10 @@
 */
 
 
+select (select max(id)+1 from tbempleados), apellidos, 'Sara', 'Gerente', 'Srta.', '1990-07-05', ciudad, pais, telefono
+from tbempleados where apellidos='King' and nombre='Robert';
+
+-- Añadir un empleado que es la hermana de Robert King, se llama Sara King, vive en el mismo domicilio, cargo "Gerente", fecha nacimiento: 12/07/1990.
+insert into tbempleados(id, apellidos, nombre, cargo, tratamiento, fechanacimiento, ciudad, pais, telefono)
+select (select max(id)+1 from tbempleados), apellidos, 'Sara', 'Gerente', 'Srta.', '1990-07-05', ciudad, pais, telefono
+from tbempleados where apellidos='King' and nombre='Robert';
