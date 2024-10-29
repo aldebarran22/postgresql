796701d95eb6e35f01576a49188977adca2ac7e5
aldebarran22 <antonioespinherranz@hotmail.com>
2024-10-29 09:25:14 +0100

    9.25

---
+++ codigo/vistas_bd_csic.sql
@@ -0,0 +1,28 @@
+/* VISTAS */ 
+
+
+-- Una vista equivalente a la tabla de public.biologicos pero utilizando las tablas del esquema local: survey, operations, biologicos
+create view local.vista_biologicos as select s.survey, s.platform_code, o.operation_code, b.species_code, b.category_code, b.sample_id, b.sampling_type
+from local.biologicos b 
+inner join local.operations o on b.idoperation = o.id
+inner join local.survey s on s.id = o.idsurvey;
+
+
+-- Prueba vista biologicos
+select * from local.vista_biologicos;
+
+
+drop view vista_bilogicos;
+
+
+-- Cuantos biologicos tenemos en cada campaña:
+select survey, count(*) as cuenta
+from local.vista_biologicos
+group by 1;
+
+-- Obtener los nombres de las species que se utilizan en la vista de biologicos:
+select distinct s.common_name as nombre
+from local.vista_biologicos v 
+inner join local.species s on v.species_code = s.species_code
+order by 1;
+
