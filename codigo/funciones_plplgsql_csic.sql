ec58857ff7bf9c8012bbc014bb35cc94b46cf5f6
aldebarran22 <antonioespinherranz@hotmail.com>
2024-10-29 12:56:36 +0100

    12.56

--- codigo/funciones_plplgsql_csic.sql
+++ codigo/funciones_plplgsql_csic.sql
@@ -6,12 +6,66 @@ as
 $$
 declare
  -- Variables locales
- porcentaje float;
+ porcentaje numeric;
 
 begin
-	porcentaje := round(parcial / total * 100.0, 2);
-	return porcentaje;
+	porcentaje := (parcial / total * 100.0)::numeric;	
+	--raise info 'porcentaje: %', porcentaje;
+	return ROUND(porcentaje, 2);	
 	
 end;
 $$
 language 'plpgsql';
+
+drop function calcularPorcentaje;
+
+
+create or replace function calcular_porcentaje(total float, parcial float) returns float
+as
+$$
+declare
+  porcentaje numeric;
+begin
+  porcentaje := (parcial / total * 100.0)::numeric;
+  return ROUND(porcentaje, 2); -- Redondea a 2 decimales
+end;
+$$
+language 'plpgsql';
+
+
+select calcular_Porcentaje(450.0, 56.7);
+
+-- Crear una función que devuelva el numero muestras por specie y categoria:
+create or replace function calcularNumMuestras(cod_specie varchar, cod_categoria integer) returns integer
+as
+$$
+declare
+	numero integer;
+begin
+	select count(*) into numero from biologicos
+	where species_code = cod_specie and category_code = cod_categoria;
+	return numero;
+end;
+$$
+language 'plpgsql';
+
+select calcularNumMuestras('10152', 0);
+
+-- Número de categorias en biologicos: mas lenta, hace el mismo cálculo varias veces
+select distinct category_code, species_code, calcularNumMuestras(species_code, category_code)  from biologicos;
+
+-- Quita primero los repetidos: más rápida
+select category_code, species_code, calcularNumMuestras(species_code, category_code)
+from (select category_code, species_code from biologicos group by 1,2);
+
+select category_code, species_code, count(*) as cuenta
+from biologicos
+group by 1,2;
+
+
+
+
+
+
+
+
