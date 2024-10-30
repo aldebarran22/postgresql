36f158a1bed476f8d5fa365af9788f4760f75c16
aldebarran22 <antonioespinherranz@hotmail.com>
2024-10-30 12:05:01 +0100

    12.04

--- codigo/plpgsql_hospital.sql
+++ codigo/plpgsql_hospital.sql
@@ -38,6 +38,49 @@ insert into personal values ('500', 4, 'Angélica', '',0,'');
 insert into personal values ('504', 4, 'Sofía', '',0,'');
 
 
+-- Bloque para repatir los datos de equipomedico_mal a dos tablas: equipocmedicos y celadores
+
+
+do $$
+declare
+	numeroCelador integer:=1;
+	equipo equipomedico_mal%rowtype;
+	enfermera1 integer; -- almacena el codigo de la enfermera1
+	enfermera2 integer; -- almacena el codigo de la enfermera2
+	
+begin
+	-- Recuperar el codigo de personal de las enfermeras:
+	select codigo into enfermera1 from personal where nombre = 'Angélica';
+	select codigo into enfermera2 from personal where nombre = 'Sofía';
+
+	raise notice 'Enfermera1: %, Enfermera2: %', enfermera1, enfermera2;
+
+	for equipo in (select * from equipomedico_mal) loop
+		raise notice 'procesando eq. medico -> id: %, idmedico: %', equipo.id, equipo.idmedico;
+
+		-- Insertar un registro en la tabla de eq. médicos. 
+		insert into equipomedico(id, idespecialidad, idmedico, idenfermera1, idenfermera2) 
+		values(equipo.id, equipo.idespecialidad, equipo.idmedico, enfermera1, enfermera2);
+
+		if equipo.idcelador1 is not null then
+			-- Si tenemos el celador1 lo copiamos a la tabla de celadores
+			insert into celadores(id, idcelador, idequipomedico) 
+			values(numeroCelador, equipo.idcelador1, equipo.idespecialidad); 
+			numeroCelador := numeroCelador + 1;
+		end if;
+
+		if equipo.idcelador2 is not null then
+			-- Si tenemos el celador2 lo copiamos a la tabla de celadores
+			insert into celadores(id, idcelador, idequipomedico) 
+			values(numeroCelador, equipo.idcelador2, equipo.idespecialidad); 
+			numeroCelador := numeroCelador + 1;
+		end if;
+		
+	end loop;
+	commit;
+
+end $$
+
 
 
 
