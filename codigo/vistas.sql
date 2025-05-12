bc75791c8c4ab12a54b57be661757fae015d77fc
aldebarran22 <antonioespinherranz@hotmail.com>
2025-05-12 10:35:29 +0200

    1035

--- codigo/vistas.sql
+++ codigo/vistas.sql
@@ -24,3 +24,35 @@ from pedidos_alemania
 group by idcliente
 order by 2 desc;
 
+-- Crear una vista para solo las bebidas;
+drop view bebidas;
+
+create or replace view bebidas as
+select p.id, p.nombre, p.idproveedor, p.idcategoria, p.preciounidad, p.unidadesenpedido, 
+p.cantidadporunidad, p.unidadesenexistencia 
+from tbproductos p
+inner join tbcategorias c
+on p.idcategoria = c.id
+where lower(c.nombre) = 'bebidas'
+
+
+select * from bebidas;
+
+-- insertar con la vista:
+-- Esto daría error porque no es una vista actualizable: tiene dos tablas!
+--insert into bebidas values(200, 'Plátanos', 1, 7, 3, 50, 'cajas de 2kg', 30);
+
+
+select * from tbproductos where id>=200;
+
+
+-- Vista actualizable:
+create or replace view solo_bebidas as
+select p.id, p.nombre, p.idproveedor, p.idcategoria, p.preciounidad, p.unidadesenpedido, 
+p.cantidadporunidad, p.unidadesenexistencia 
+from tbproductos p
+where p.idcategoria = 1;
+
+insert into solo_bebidas values(201, 'Naranjas', 1, 7, 3, 50, 'cajas de 2kg', 30);
+
+
