cd8c4534545eb7b6951dbe2c4610b1f5b80fa283
aldebarran22 <antonioespinherranz@hotmail.com>
2025-05-13 15:57:56 +0200

    16

--- codigo/script_paises_empresa3.sql
+++ codigo/script_paises_empresa3.sql
@@ -38,9 +38,88 @@ where pa.nombre = pe.paisdestinatario;
 
 -- Comprobar si se han actualizado correctamente los paises: tbpedidos2.idpais
 -- Contar los registros que no coincide el nombre del país (tbpaises) con el paisdestinatario de tbpedidos2
+/*
+select count(*) as errores
+from tbpedidos2 pe 
+inner join tbpaises pa on pe.idpais = pa.id
+where pa.nombre != pe.paisdestinatario;
+
+select pe.id, pe.idpais, pe.paisdestinatario, pa.id, pa.nombre
+from tbpedidos2 pe 
+inner join tbpaises pa on pe.idpais = pa.id order by 1;*/
+
+-- Añadir una clave primaria a tbpedidos2:
+alter table if exists tbpedidos2 
+add primary key (id);
+
+
+-- Borrar la columna paisdestinatario de tbpedidos2:
+alter table if exists tbpedidos2 
+drop column paisdestinatario;
+
+
+-- Añadir claves foráneas: idcliente, idempleado, idcompanya, idpais
+-- Añadir la claves foraneas en la tabla de tbpedidos2;
+ALTER TABLE IF EXISTS public.tbpedidos2
+    ADD CONSTRAINT "fk-compañias" FOREIGN KEY (idcompanya)
+    REFERENCES public.tbcompaniaenvios (id) MATCH SIMPLE
+    ON UPDATE RESTRICT
+    ON DELETE RESTRICT
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public.tbpedidos2
+    ADD CONSTRAINT "fk-clientes" FOREIGN KEY (idcliente)
+    REFERENCES public.tbclientes (id) MATCH SIMPLE
+    ON UPDATE RESTRICT
+    ON DELETE RESTRICT
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public.tbpedidos2
+    ADD CONSTRAINT "fk-empleado" FOREIGN KEY (idempleado)
+    REFERENCES public.tbempleados (id) MATCH SIMPLE
+    ON UPDATE RESTRICT
+    ON DELETE RESTRICT
+    NOT VALID;
+
+-- LA NUEVA FK PARA LOS PAISES:
+ALTER TABLE IF EXISTS public.tbpedidos2
+    ADD CONSTRAINT "fk-pais" FOREIGN KEY (idpais)
+    REFERENCES public.tbpaises (id) MATCH SIMPLE
+    ON UPDATE RESTRICT
+    ON DELETE RESTRICT
+    NOT VALID;
+
+
+/*
+-- Reconectar detalles de pedidos con la nueva tabla tbpedidos2:
+-- Borrar la FK de detalles de pedidos que se relaciona con tbpedidos:
+alter table if exists public.tbdetallespedidos drop constraint if exists "fk-pedidos";
+
+
+-- Añadir una nueva FK en detalles de pedidos que esté vinculada con tbpedidos2
+ALTER TABLE IF EXISTS public.tbdetallespedidos
+    ADD CONSTRAINT "fk-pedidos" FOREIGN KEY (idpedido)
+    REFERENCES public.tbpedidos2 (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+*/
+	
+
+
+--create schema if not exists copia;
+
+--create table if not exists copia.tbpedidos as (select * from public.tbpedidos);
 
 
 
+SELECT conname AS nombre_fk, conrelid::regclass AS tabla_origen,
+       confrelid::regclass AS tabla_referenciada
+FROM pg_constraint
+WHERE contype = 'f' AND conrelid = 'tbdetallespedidos'::regclass and conname = 'fk-pedidos';
 
+select * from pg_tables where tablename = 'tbpedidos2';
 
 
